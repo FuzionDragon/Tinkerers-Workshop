96a33ed14ebdd95d79347248e9104468584682c7
David L <120467347+FuzionDragon@users.noreply.github.com>
2024-12-21 22:50:15 +0000

    Changes to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({
+  base: "/FuzionDragon.github.io/",
   plugins: [suidPlugin(), solidPlugin()],
   build: {
     target: "esnext",
