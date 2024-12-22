ada0d229e93aa27ba755371af5b219200be65506
David L <120467347+FuzionDragon@users.noreply.github.com>
2024-12-22 12:17:27 +0000

    Adding Netlify requirements

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  start: {
-    ssr: true,
-    server: {
-      baseURL: process.env.BASE_PATH,
-      preset: "static",
-    }
+  server: {
+    preset: "netlify",
   }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import suidPlugin from "@suid/vite-plugin";
+import netlify from "solid-start-netlify";
 
 export default defineConfig({
   base: "/Tinkerers-Workshop/",
-  plugins: [suidPlugin(), solidPlugin()],
+  plugins: [
+    suidPlugin(), 
+    solidPlugin(),
+    solid({
+      adapter: netlify({edge: true}),
+    }),
+  ],
   build: {
     target: "esnext",
   },
